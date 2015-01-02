1928dbaedef6097f433e53e84a6bf5e672491e55
Graham Howden <graham_howden1@yahoo.co.uk>
2015-01-02 09:56:22 +0000

    refactor(index): move annonymous functions into proper functions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ var Snoocore = require('snoocore');
 var config = require('../config/config.json');
 var TitleFormatPass = require('./passes/TitleFormatPass');
 var logger = require('./Logger');
+var Q = require('q');
 
 var reddit = new Snoocore({
     userAgent: 'UltraHardcore Flair Checker',
@@ -12,19 +13,47 @@ var reddit = new Snoocore({
 
 var formatPass = new TitleFormatPass(reddit);
 
-logger.info('Starting up database');
-require('./db/DbInit')().then(function() {
-    return formatPass.removeOld()
-}).then(function() {
+initDatabase()
+    .then(authenticate)
+    .then(titlePass)
+    .catch(function(err) {
+        logger.error('Uncaught error during scripts: %s', err);
+    });
+
+/**
+ * Pass that works once for each post to check titles. Skips any previously parsed posts
+ *
+ * @returns {Q.promise}
+ */
+function titlePass() {
+    logger.info('Starting title based pass');
+    return reddit('/r/$subreddit/search').get(config.titlePass.query).then(function(results) {
+        logger.info('Found %d posts to check', results.data.children.length);
+
+        logger.info('Starting title format check');
+        return formatPass.checkPosts(results.data.children);
+    });
+}
+
+/**
+ * Authenticate the account with reddit OAuth
+ *
+ * @returns {Q.promise}
+ */
+function authenticate() {
     logger.info('Authenticating with Reddit');
-    return reddit.auth()
-}).then(function() {
-    logger.info('Fetching reddit posts');
-    return reddit('/r/$subreddit/search').get(config.titlePass.query);
-}).then(function(results) {
-    logger.info('Found %d posts to check', results.data.children.length);
-    logger.info('Starting title format check');
-    return formatPass.checkPosts(results.data.children);
-}).catch(function(err) {
-    logger.error('Uncaught error during script: %s', err);
-});
+
+    return reddit.auth();
+}
+
+/**
+ * Sets up the database and syncs models structure and then removes all the expired post checks from the database
+ *
+ * @returns {Q.promise}
+ */
+function initDatabase() {
+    logger.info('Starting up database');
+
+    return require('./db/DbInit')()
+        .then(formatPass.removeOld)
+}
