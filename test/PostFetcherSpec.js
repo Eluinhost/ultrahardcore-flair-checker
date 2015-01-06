5445b95379566ccb434c12f6a94c1865bc51e6a7
Graham Howden <graham_howden1@yahoo.co.uk>
2015-01-06 15:53:21 +0000

    fix(post fetcher): neater error catching for tests

--- test/PostFetcherSpec.js
+++ test/PostFetcherSpec.js
@@ -53,28 +53,23 @@ describe('PostFetcher', function () {
         // fetch 70 posts
         fetcher._fetch('sub', 'query', 70).then(
             function(data) {
-                try {
-                    expect(reddit).to.have.been.calledWithExactly('/r/$subreddit/search');
-                    expect(listing).to.have.been.calledWithExactly({
-                        $subreddit: 'sub',
-                        q: 'query',
-                        restrict_sr: true,
-                        sort: 'new',
-                        count: 70  // ensure we're fetching 70
-                    });
-                    expect(fullSlice.next).to.not.have.been.called; // no calls to the next page should happen
-                    expect(shortSlice.next).to.not.have.been.called;
-                    expect(data).to.have.length(70); // make sure we get the correct amount
+                expect(reddit).to.have.been.calledWithExactly('/r/$subreddit/search');
+                expect(listing).to.have.been.calledWithExactly({
+                    $subreddit: 'sub',
+                    q: 'query',
+                    restrict_sr: true,
+                    sort: 'new',
+                    count: 70  // ensure we're fetching 70
+                });
+                expect(fullSlice.next).to.not.have.been.called; // no calls to the next page should happen
+                expect(shortSlice.next).to.not.have.been.called;
+                expect(data).to.have.length(70); // make sure we get the correct amount
 
-                    done();
-                } catch(e) {
-                    done(e);
-                }
-            },
-            function(err) {
-                done('Error: ' + err);
+                done();
             }
-        );
+        ).fail(function(err) {
+            done(err);
+        });
     });
 
     // make sure it queries the next page when fetching > 100 posts
@@ -83,29 +78,24 @@ describe('PostFetcher', function () {
         // start a query for 110 posts
         fetcher._fetch('sub', 'query', 110).then(
             function(data) {
-                try {
-                    expect(reddit).to.have.been.calledWithExactly('/r/$subreddit/search');
-                    expect(listing).to.have.been.calledWithExactly({
-                        $subreddit: 'sub',
-                        q: 'query',
-                        restrict_sr: true,
-                        sort: 'new',
-                        count: 100  // 100 is max for reddit so we do it in chunks
-                    });
-                    expect(fullSlice.next).to.have.been.calledOnce; // only query the first slice after the listing
-                    expect(shortSlice.next).to.have.not.been.called;
+                expect(reddit).to.have.been.calledWithExactly('/r/$subreddit/search');
+                expect(listing).to.have.been.calledWithExactly({
+                    $subreddit: 'sub',
+                    q: 'query',
+                    restrict_sr: true,
+                    sort: 'new',
+                    count: 100  // 100 is max for reddit so we do it in chunks
+                });
+                expect(fullSlice.next).to.have.been.calledOnce; // only query the first slice after the listing
+                expect(shortSlice.next).to.have.not.been.called;
 
-                    // check length of returned data is the same we requested
-                    expect(data).to.have.length(110);
-                    done();
-                } catch(e) {
-                    done(e);
-                }
-            },
-            function(err) {
-                done('Error: ' + err);
+                // check length of returned data is the same we requested
+                expect(data).to.have.length(110);
+                done();
             }
-        );
+        ).fail(function(err) {
+            done(err);
+        });
     });
 
     // we only have 125 posts available, make sure if we query for more we return all possible
@@ -114,29 +104,24 @@ describe('PostFetcher', function () {
         // start a query for 150 posts
         fetcher._fetch('sub', 'query', 150).then(
             function(data) {
-                try {
-                    expect(reddit).to.have.been.calledWithExactly('/r/$subreddit/search');
-                    expect(listing).to.have.been.calledWithExactly({
-                        $subreddit: 'sub',
-                        q: 'query',
-                        restrict_sr: true,
-                        sort: 'new',
-                        count: 100  // 100 is max for reddit so we do it in chunks
-                    });
-                    expect(fullSlice.next).to.have.been.calledOnce;  // we query for the next page
-                    expect(shortSlice.next).to.have.not.been.called;  // there are no more pages after the short one so it shouldn't query for the next page
+                expect(reddit).to.have.been.calledWithExactly('/r/$subreddit/search');
+                expect(listing).to.have.been.calledWithExactly({
+                    $subreddit: 'sub',
+                    q: 'query',
+                    restrict_sr: true,
+                    sort: 'new',
+                    count: 100  // 100 is max for reddit so we do it in chunks
+                });
+                expect(fullSlice.next).to.have.been.calledOnce;  // we query for the next page
+                expect(shortSlice.next).to.have.not.been.called;  // there are no more pages after the short one so it shouldn't query for the next page
 
-                    // ensure we got all of the data
-                    expect(data).to.have.length(125);
-                    done();
-                } catch(e) {
-                    done(e);
-                }
-            },
-            function(err) {
-                done('Error: ' + err);
+                // ensure we got all of the data
+                expect(data).to.have.length(125);
+                done();
             }
-        )
+        ).fail(function(err) {
+            done(err);
+        });
     });
 
     it('deduplicates repeat post IDs', function(done) {
@@ -147,29 +132,24 @@ describe('PostFetcher', function () {
         // start a query for 250 posts (fetches 100->100->25 for 225 and 115 after deduplication)
         fetcher._fetch('sub', 'query', 250).then(
             function(data) {
-                try {
-                    expect(reddit).to.have.been.calledWithExactly('/r/$subreddit/search');
-                    expect(listing).to.have.been.calledWithExactly({
-                        $subreddit: 'sub',
-                        q: 'query',
-                        restrict_sr: true,
-                        sort: 'new',
-                        count: 100  // 100 is max for reddit so we do it in chunks
-                    });
-                    expect(fullSlice.next).to.have.been.calledTwice;  // we query for the next page
-                    expect(shortSlice.next).to.have.not.been.called;  // there are no more pages after the short one so it shouldn't query for the next page
+                expect(reddit).to.have.been.calledWithExactly('/r/$subreddit/search');
+                expect(listing).to.have.been.calledWithExactly({
+                    $subreddit: 'sub',
+                    q: 'query',
+                    restrict_sr: true,
+                    sort: 'new',
+                    count: 100  // 100 is max for reddit so we do it in chunks
+                });
+                expect(fullSlice.next).to.have.been.calledTwice;  // we query for the next page
+                expect(shortSlice.next).to.have.not.been.called;  // there are no more pages after the short one so it shouldn't query for the next page
 
-                    // ensure we got all of the data
-                    expect(data).to.have.length(125);
-                    done();
-                } catch(e) {
-                    done(e);
-                }
-            },
-            function(err) {
-                done('Error: ' + err);
+                // ensure we got all of the data
+                expect(data).to.have.length(125);
+                done();
             }
-        )
+        ).fail(function(err) {
+            done(err);
+        });
     });
 
     it('fetches unflaired posts', function(done) {
@@ -179,19 +159,17 @@ describe('PostFetcher', function () {
         fetcher._fetch.returns(def.promise);
 
         fetcher.fetchUnflaired(240).then(function() {
-            try {
-                var ignore = ['ig1', 'ig2', 'upcoming', 'invalid', 'completed'];
+            var ignore = ['ig1', 'ig2', 'upcoming', 'invalid', 'completed'];
 
-                var query = ignore.reduce(function(prev, current) {
-                    return prev + '-flair:' + current + ' ';
-                }, '');
+            var query = ignore.reduce(function(prev, current) {
+                return prev + '-flair:' + current + ' ';
+            }, '');
 
-                expect(fetcher._fetch).to.have.been.calledWithExactly('sub', query, 240);
+            expect(fetcher._fetch).to.have.been.calledWithExactly('sub', query, 240);
 
-                done();
-            } catch(e) {
-                done(e);
-            }
+            done();
+        }).fail(function(err) {
+            done(err);
         });
     });
 
@@ -202,13 +180,11 @@ describe('PostFetcher', function () {
         fetcher._fetch.returns(def.promise);
 
         fetcher.fetchUpcoming(240).then(function() {
-            try {
-                expect(fetcher._fetch).to.have.been.calledWithExactly('sub', 'flair:upcoming', 240);
+            expect(fetcher._fetch).to.have.been.calledWithExactly('sub', 'flair:upcoming', 240);
 
-                done();
-            } catch(e) {
-                done(e);
-            }
+            done();
+        }).fail(function(err) {
+            done(err);
         });
     });
 });
